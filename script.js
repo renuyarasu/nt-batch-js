3ab5d1e9c04c6a54fc023b2043ab6b1513b5c62f
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-24 22:12:16 +0530

    Array: slice

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ console.clear();
 
 let newArray = [1, 2, 3, 4, 5];
 
-newArray.forEach(function (val, index) {
-    console.log(val);
-})
-/* 
-1
-2
-3
-4
-5 */
+let result = newArray.slice(1, 5);
+
+console.log(result);
