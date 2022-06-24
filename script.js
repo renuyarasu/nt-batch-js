0c68fc0e80d63d5d5d228cdc464e8dcefbf98a1f
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-24 22:15:29 +0530

    Array: some

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let newArray = [1, 2, 3, 4, 5];
 
 // Find > 2 value
 
-let result = newArray.filter(function (val, index) {
+let result = newArray.some(function (val, index) {
     return val > 2
 })
 
-console.log(result); //[ 3, 4, 5 ]
+console.log(result); // true
